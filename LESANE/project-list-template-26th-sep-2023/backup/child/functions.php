3fe306eed7b953c33e1d52b0a8c1b5ea7042798e
trunghieu7088 <lamtrunghieu366@gmail.com>
2023-09-26 19:25:26 +0700

    upload project list new template Lesane

---
+++ LESANE/project-list-template-26th-sep-2023/backup/child/functions.php
@@ -0,0 +1,3 @@
+<?php
+require('customfunction.php');
+require('add_category_option.php');
---
+++ LESANE/project-list-template-26th-sep-2023/upload/child/functions.php
@@ -0,0 +1,4 @@
+<?php
+require('customfunction.php');
+require('add_category_option.php');
+require('custom_require_files.php');

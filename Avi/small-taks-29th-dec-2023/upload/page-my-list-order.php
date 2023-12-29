0343084ebb4b724b11ad7db57dbee3d7d751e96d
tranbao <lamtrunghieu366@gmail.com>
2023-12-29 21:55:15 +0700

    small task 29 dec 2023
    small task 29 dec 2023

---
+++ Avi/small-taks-29th-dec-2023/upload/page-my-list-order.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Template Name: My orders listing
+ *
+ * @param void
+ * @return void
+ * @since 1.0
+ * @package MicrojobEngine
+ * @category void
+ * @author JACK BUI
+ */
+
+
+get_header();
+
+    //custom code here
+    $custom_user = get_userdata( get_current_user_id() );
+    if ( in_array( 'client', $custom_user->roles, true ) ) {
+      $is_client=true;
+    
+    }
+    else
+    {
+        $is_client=false;
+     
+    }
+    
+    //end
+
+?>
+<div id="content" class="my-list-order">
+    <div class="block-page">
+        <div class="container dashboard withdraw">
+            <div class="row title-top-pages">
+                <p class="block-title"><?php _e('MY ORDERS & TASKS', 'enginethemes'); ?></p>
+                <p><a href="<?php echo et_get_page_link('dashboard'); ?>" class="btn-back"><i class="fa fa-angle-left"></i><?php _e('Back to dashboard', 'enginethemes'); ?></a></p>
+            </div>
+             <div class="row profile">
+                 <div class="col-lg-3 col-md-3 col-sm-12 col-sx-12 profile">
+                    <?php get_sidebar('my-profile'); ?>
+                 </div>
+                 <div class="col-lg-9 col-md-9 col-sm-12 col-sx-12 outer-revenues">
+                    <div class="information-items-detail box-shadow">
+                        <div class="tabs-information">
+
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li role="presentation" class="active"><a href="#order" aria-controls="order" role="tab" data-toggle="tab"><?php _e('My order', 'enginethemes'); ?></a></li>
+                                <?php if($is_client==false): ?>
+                                    <li role="presentation"><a href="#task" aria-controls="task" role="tab" data-toggle="tab"><?php _e('Tasks', 'enginethemes'); ?></a></li>
+                                <?php endif; ?>
+                            </ul>
+                            <div class="tab-content">
+                                <div role="tabpanel" class="tab-pane active order-container-control" id="order">
+                                    <div class="filter-order">
+                                        <?php get_template_part('template/filter', 'order'); ?>
+                                    </div>
+                                    <?php get_template_part('template/dashboard-list', 'orders'); ?>
+                                    </form>
+                                </div>
+                                <?php if($is_client==false): ?>
+                                    <div role="tabpanel" class="tab-pane task-container-control" id="task">
+                                        <div class="filter-order">
+                                            <?php get_template_part('template/filter', 'task'); ?>
+                                        </div>
+                                        <?php get_template_part('template/dashboard-list', 'tasks'); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+
+                        </div>
+                    </div>
+<!--                    <div class="load-more">-->
+<!--                        <a href="" class="link-more">Load more<i class="fa fa-angle-right"></i></a>-->
+<!--                    </div>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+get_footer();
---
+++ Avi/small-taks-29th-dec-2023/upload/template/mjob-item.php
@@ -0,0 +1,107 @@
+<div class="<?php echo $current->mjob_class; ?> ">
+    <?php
+    /**
+     * Fire action before mjob item
+     *
+     * @param object $current
+     * @since 1.3.1
+     * @author Tat Thien
+     */
+    do_action('mje_mjob_item_top', $current);
+    ?>
+    <div class="mjob-item__image">
+        <?php
+        /**
+         * Fire action before mjob image
+         *
+         * @param object $current
+         */
+        do_action('mje_mjob_item_before_image', $current);
+        ?>
+
+        <a href="<?php echo $current->permalink; ?>" class="<?php echo $current->class_video; ?>">
+            <?php echo $current->mje_get_thumbnail; ?>
+        </a>
+       
+        <?php
+        /**
+         * Fire action after mjob image
+         *
+         * @param object $current
+         */
+        do_action('mje_mjob_item_after_image', $current);
+        ?>
+    </div><!-- end .mjob-item__image -->
+    <p class="custom-author-name-mjob"> <?php echo $current->author_name; ?> </p>
+    <!-- custom code -->
+    <?php       
+         $short_content=wp_trim_words($current->post_content,14,'..');
+           if (isset($current->tax_input['location']) && !empty($current->tax_input['location'])) 
+            {
+                $tax = $current->tax_input['location'];
+                if (isset($tax[0])) {
+                    $location = $tax[0]->name;
+                    $show_location = 'Duration: '.$location;                   
+                }            
+            }
+            else
+            {
+                $show_location = 'Duration: None';                   
+            }
+        	
+    ?>
+    <!-- end custom code -->
+
+    <div class="mjob-item__entry">
+        <div class="mjob-item__title">
+            <h2 class="trimmed" title="<?php echo $current->post_title; ?>">
+                <a href="<?php echo $current->permalink; ?>"><?php echo $current->post_title; ?></a>
+            </h2>
+            <div class="custom-description-mjob">
+                <?php echo $short_content; ?>
+            </div>
+        </div><!-- end .mjob-item__title -->
+
+        <div class="mjob-item__title">
+        </div>
+        
+
+       <!-- <div class="mjob-item__author trimmed">
+            <span title="<?php echo $current->author_name; ?>"><?php echo $current->author_name; ?></span>
+        </div> --><!-- end .mjob-item__author -->
+
+      
+
+        <div class="mjob-item__price">
+            <div class="mjob-item__price-inner">
+                <!-- <span class="starting-text"><i class="fa fa-eye" aria-hidden="true"></i><?php //echo $current->view_count ? $current->view_count : 0 ?></span> -->
+                <span class="custom-duration-mjob"><?php echo $show_location; ?></span>
+                <span class="price-text customize-color"><?php echo $current->et_budget_text ?></span>
+            </div>
+        </div><!-- end .mjob-item__price -->
+
+        <div class="mjob-item__bottom clearfix">
+            <?php
+            /**
+             * Fire action before mjob rating
+             *
+             * @author Tat Thien
+             */
+            do_action('mje_mjob_item_before_rating', $current);
+            ?>
+            <div class="mjob-item__rating">
+                <div class="rate-it star" data-score="<?php echo $current->rating_score; ?>"></div>
+                <span class="total-review"><?php printf('(%s)', $current->mjob_total_reviews); ?></span>
+            </div><!-- end .mjob-item__ratings -->
+        </div>
+    </div>
+
+    <?php
+    /**
+     * Fire action after mjob item
+     *
+     * @param object $current
+     */
+    do_action('mje_mjob_item_bottom', $current);
+    ?>
+</div>

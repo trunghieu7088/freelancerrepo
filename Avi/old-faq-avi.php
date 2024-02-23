b18ee1dc35ff02c69f8bc2dc48fa025441b42b09
tranbao <lamtrunghieu366@gmail.com>
2024-02-23 10:44:36 +0700

    create old-avi-faq.php backup

---
+++ Avi/old-faq-avi.php
@@ -0,0 +1,83 @@
+ <!-- profile faq -->
+   <div class="container profilepage-faq-container">
+         <!-- <p class="profilepage-faq-title">Frequently Asked Questions</p> -->                          
+      <div class="row profilepage-faq-content-wrapper">
+
+         <div class="col-md-5 col-lg-5">
+               <div class="profilepage-faq-image">
+                     <img src="http://avi.et/wp-content/uploads/2024/02/pngtree-male-airline-mechanic-repairing-aircraft-in-hangar-airline-wrench-vertical-photo-image_44602642.jpg">                 
+               </div>
+         </div>
+
+         <div class="col-md-7 col-lg-7 col-sm-12">                             
+               <div class="profilepage-faq-questions-container">
+               <div class="profilepage-faq-title">Frequently Asked Questions</div>
+                  <!-- question item -->                     
+                    <div class="profilepage-faq-question">
+                        <p data-toggle="collapse" data-target="#question1" class="profilepage-faq-question-title">
+                           <span>How can I book a session ?  </span> 
+                           <span class="profilepage-faq-question-title-arrow"><i class="fa fa-chevron-down"></i></span>                     
+                        </p>
+                        <div id="question1" class="profilepage-faq-answer collapse">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters            
+                        </div>                          
+                    </div>
+                  <!-- end question item -->  
+                  
+                  <div class="profilepage-faq-question">
+                        <p data-toggle="collapse" data-target="#question2" class="profilepage-faq-question-title">
+                           <span>How can I get refund ?  </span> 
+                           <span class="profilepage-faq-question-title-arrow"><i class="fa fa-chevron-down"></i></span>                     
+                        </p>
+                        <div id="question2" class="profilepage-faq-answer collapse">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters            
+                        </div>                          
+                    </div>
+
+                    <div class="profilepage-faq-question">
+                        <p data-toggle="collapse" data-target="#question3" class="profilepage-faq-question-title">
+                           <span>How can I pay ?  </span> 
+                           <span class="profilepage-faq-question-title-arrow"><i class="fa fa-chevron-down"></i></span>                     
+                        </p>
+                        <div id="question3" class="profilepage-faq-answer collapse">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters            
+                        </div>                          
+                    </div>
+
+                    <div class="profilepage-faq-question">
+                        <p data-toggle="collapse" data-target="#question4" class="profilepage-faq-question-title">
+                           <span>What is your policy ? </span> 
+                           <span class="profilepage-faq-question-title-arrow"><i class="fa fa-chevron-down"></i></span>                     
+                        </p>
+                        <div id="question4" class="profilepage-faq-answer collapse">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters            
+                        </div>                          
+                    </div>
+
+                    <div class="profilepage-faq-question">
+                        <p data-toggle="collapse" data-target="#question6" class="profilepage-faq-question-title">
+                           <span>How do I know the price ? </span> 
+                           <span class="profilepage-faq-question-title-arrow"><i class="fa fa-chevron-down"></i></span>                     
+                        </p>
+                        <div id="question6" class="profilepage-faq-answer collapse">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters            
+                        </div>                          
+                    </div>
+
+                    <div class="profilepage-faq-question">
+                        <p data-toggle="collapse" data-target="#question5" class="profilepage-faq-question-title">
+                           <span>Is the expert good ? </span> 
+                           <span class="profilepage-faq-question-title-arrow"><i class="fa fa-chevron-down"></i></span>                     
+                        </p>
+                        <div id="question5" class="profilepage-faq-answer collapse">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters            
+                        </div>                          
+                    </div>
+                  
+
+               </div>                     
+         </div>
+
+      </div>
+   </div>                                 
+   <!-- end profile faq -->
